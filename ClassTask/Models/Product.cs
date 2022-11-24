d0e258872be213d0506470fd41e5844440617c4e
Asmar003 <esmer.ceferova24@gamil.com>
2022-11-24 08:49:06 +0400

    commit

---
+++ ClassTask/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask.Models
+{
+    internal class Product
+    {
+        string _name;
+        float _price;
+
+        public Product(string name, float price)
+        {
+            Name = name;
+            Price = price;
+          
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length>5 && value.Length < 25)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public float Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value > 0)
+                {
+                    _price=value;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ ClassTask/Models/User.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask.Models
+{
+    internal class User
+    {
+        string _username;
+        string _password;
+        bool _isSignedIn;
+        Product[] products;
+
+        public string UserName
+        {
+            get { return _username; }
+            set
+            {
+                if(value.Length>5 && value.Length <= 20)
+                {
+                    _username = value;
+                }
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if(value.Length > 7 && value.Length <= 20)
+                {
+                    _password = value;
+                }
+            }
+        }
+
+        public bool IsSignedIn
+        {
+            get { return _isSignedIn; }
+            set { _isSignedIn = value; }
+            
+        }
+        
+        public Product[] Products
+        {
+            get { return Products; }
+            set
+            {
+                Products = value;
+            }
+        }
+
+        public static bool HasDigit(string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsDigit(value[i]))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        public static bool HasUpper(string value)
+        {
+            for (var i = 0; i < value.Length; i++)
+            {
+                if (char.IsUpper(value[i]))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        public static bool HasLower(string value)
+        {
+            for (var i = 0; i < value.Length; i++)
+            {
+                if (char.IsLower(value[i]))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        public static void Login()
+        {
+
+        }
+
+
+    }
+}
